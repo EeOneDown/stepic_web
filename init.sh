3ae4e0abe54e2005ff1fe68e6be3d4cf04496a66
Ivan Platonov <st049378@student.spbu.ru>
2019-02-02 19:44:52 +0300

    yet another fixes

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
-gunicorn -c hello.py hello:app
+sudo gunicorn -c hello.py -b 0.0.0.0:8080 hello:app
