c165e8c0c60f29449777376daf3ea165755ce495
Ivan Platonov <st049378@student.spbu.ru>
2019-02-02 19:12:43 +0300

    nginx fixes

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
-sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+gunicorn -c hello.py hello:app
