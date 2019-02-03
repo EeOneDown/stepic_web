59e4d5067ee120489e76ca7d62619e150df8465a
Ivan Platonov <st049378@student.spbu.ru>
2019-02-03 12:11:23 +0300

    some updates

--- init.sh
+++ init.sh
@@ -2,10 +2,11 @@
 
 sudo pip3 install -r requirements.txt
 
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/gunicorn-django.conf
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
 # sudo gunicorn -c hello.py -b 0.0.0.0:8080 hello:app &
 sudo gunicorn -c gunicorn-django.conf -b 0.0.0.0:8000 ask.wsgi:application
