7df6643c409f51f6688c18db6c069c1917852359
Ivan Platonov <st049378@student.spbu.ru>
2019-02-03 12:24:28 +0300

    django path to url changed

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.not_found, name='index'),
-    path('login/', views.not_found, name='login'),
-    path('signup/', views.not_found, name='signup'),
-    path('question/(?P<pk>\d+)/', views.test, name='question'),
-    path('ask/', views.not_found, name='ask'),
-    path('popular/', views.not_found, name='popular'),
-    path('new', views.not_found, name='new'),
+    url(r'^$', views.not_found, name='index'),
+    path(r'^login/$', views.not_found, name='login'),
+    path(r'^signup/$', views.not_found, name='signup'),
+    path(r'^question/(?P<pk>\d+)/$', views.test, name='question'),
+    path(r'^ask/$', views.not_found, name='ask'),
+    path(r'^popular/$', views.not_found, name='popular'),
+    path(r'^new$', views.not_found, name='new'),
 ]
