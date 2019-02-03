8c1a2cb593ff11fc7bc42d7f68fe380a7237986c
Ivan Platonov <st049378@student.spbu.ru>
2019-02-03 12:29:17 +0300

    urls fixes

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.not_found, name='index'),
-    path(r'^login/$', views.not_found, name='login'),
-    path(r'^signup/$', views.not_found, name='signup'),
-    path(r'^question/(?P<pk>\d+)/$', views.test, name='question'),
-    path(r'^ask/$', views.not_found, name='ask'),
-    path(r'^popular/$', views.not_found, name='popular'),
-    path(r'^new$', views.not_found, name='new'),
+    url(r'^login/$', views.not_found, name='login'),
+    url(r'^signup/$', views.not_found, name='signup'),
+    url(r'^question/(?P<pk>\d+)/$', views.test, name='question'),
+    url(r'^ask/$', views.not_found, name='ask'),
+    url(r'^popular/$', views.not_found, name='popular'),
+    url(r'^new$', views.not_found, name='new'),
 ]
