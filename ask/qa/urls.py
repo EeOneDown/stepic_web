ac2b5c8e6e09c75bc31231b9fcf8cf3287143118
Ivan Platonov <st049378@student.spbu.ru>
2019-02-03 12:46:53 +0300

    new http requests

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.not_found, name='index'),
-    url(r'^login/$', views.not_found, name='login'),
-    url(r'^signup/$', views.not_found, name='signup'),
+    url(r'^$', views.test, name='index'),
+    url(r'^login/$', views.test, name='login'),
+    url(r'^signup/$', views.test, name='signup'),
     url(r'^question/(?P<pk>\d+)/$', views.test, name='question'),
-    url(r'^ask/$', views.not_found, name='ask'),
-    url(r'^popular/$', views.not_found, name='popular'),
-    url(r'^new$', views.not_found, name='new'),
+    url(r'^ask/$', views.test, name='ask'),
+    url(r'^popular/$', views.test, name='popular'),
+    url(r'^new$', views.test, name='new'),
 ]
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -3,7 +3,3 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
-
-
-def not_found(request):
-    return HttpResponseNotFound()
