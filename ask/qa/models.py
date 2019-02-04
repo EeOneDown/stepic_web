783b828c695db4c039ea748af43fc980fb4c5d8f
Ivan Platonov <st049378@student.spbu.ru>
2019-02-04 23:11:57 +0300

    django db

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating')
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    added_at = models.DateField(blank=True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(
+        User, related_name='q_to_u', on_delete=models.CASCADE
+    )
+    likes = models.ManyToManyField(User, related_name='q_to_l')
+
+    objects = QuestionManager()
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField()
+    question = models.ForeignKey(
+        Question, related_name='a_to_q', on_delete=models.CASCADE
+    )
+    author = models.ForeignKey(
+        User, related_name='a_to_u', on_delete=models.CASCADE
+    )
--- init.sh
+++ init.sh
@@ -8,7 +8,15 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
+
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE djbase;"
+mysql -uroot -e "CREATE USER 'djuser@localhost' IDENTIFIED BY 'djangopass';"
+mysql -uroot -e "GRANT ALL ON djbase.* TO 'djuser@localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
+
 # sudo gunicorn -c test-wsgi -b 0.0.0.0:8080 hello:app &
-# cd ask
-# gunicorn -c ../etc/gunicorn-django.conf -b 0.0.0.0:8000 ask.wsgi:application
-sudo gunicorn -c test-django -b 0.0.0.0:8000 ask.wsgi:application
+cd ask
+venv/bin/python manage.py migrate
+gunicorn -c ../etc/gunicorn-django.conf -b 0.0.0.0:8000 ask.wsgi:application
+# sudo gunicorn -c test-django -b 0.0.0.0:8000 ask.wsgi:application
